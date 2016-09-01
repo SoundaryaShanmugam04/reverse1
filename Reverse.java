44faf4ded57192120b2c3ae2a30ac7c1d462ed04
SoundaryaShanmugam04 <suk.ietcse13@gmail.com>
2016-09-01 20:58:43 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Reverse {
+	public static void main(String[] args) {
+		String str="soundarya";
+		StringBuffer sb=new StringBuffer(str);
+		System.out.println(sb.reverse());
+		
+		
+		
+		
+		
+	}
+
+}
